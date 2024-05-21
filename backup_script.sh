73628953c2b68c887c5c20f41f8f54515cc1ac37
Ferdy Sopian <pinoan42@gmail.com>
2024-05-22 01:16:12 +0700

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -44,11 +44,22 @@ fi
 # Remove local backup files
 rm $BACKUP_DIR/www-backup-$DATE.tar.gz
 
-# Remove backups older than 7 days in Dropbox
-$DROPBOX_UPLOADER_PATH delete $DROPBOX_BACKUP_DIR* -t 7
+# List all files in DROPBOX BACKUP DIR and check/Remove backups older than 7 days in Dropbox
+$DROPBOX_UPLOADER_PATH list $DROPBOX_BACKUP_DIR | grep -E 'backup-[0-9]{4}-[0-9]{2}-[0-9]{2}.tar.gz' | while read -r line; do
+    # Extract the filename (the 3rd field) from the line
+    FILENAME=$(echo $line | awk '{print $3}')
+    
+    # Extract the date from the filename
+    FILE_DATE=$(echo $FILENAME | grep -o -E '[0-9]{4}-[0-9]{2}-[0-9]{2}')
+
+    # If the file is older than 7 days, delete it
+    if [ $(date -d $FILE_DATE +%s) -lt $(date -d '7 days ago' +%s) ]; then
+        $DROPBOX_UPLOADER_PATH delete $DROPBOX_BACKUP_DIR/$FILENAME
+    fi
+done
 
 echo "Backup and upload completed successfully."
 
-# Schedule this script to run weekly using cron:
+# Schedule this script to run daily at 2 AM using cron:
 # Run 'crontab -e' and add the following line:
-# 0 2 * * 0 /path/to/backup_script.sh
+# 0 2 * * * /path/to/backup_script.sh >> /path/to/backup_script.log 2>&1
